72f69c1e2f946ebe093febadd54cbc4c347e1789
BONIFACE KORIR <boniface.korir@student.moringa.school@gmail.com>
2023-02-08 10:40:10 +0300

    Fixed setter and getters

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+   def name=(name)
+    @name = name
+  end
 
+    def name
+        @name
+    end
+
+        def breed=(breed_type)
+            @breed = breed_type
+        end
+
+        def breed
+            @breed
+        end 
 end
+
